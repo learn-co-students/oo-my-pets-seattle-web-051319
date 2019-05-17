7c2cef47f5224de3a9541158fd8ca0e1b38f48fa
Chet  Chopra <chopra.chet@gmail.com>
2019-05-17 00:01:01 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,79 @@
+require 'pry'
 class Owner
-  # code goes here
+  
+  attr_reader :species
+  attr_accessor :name, :pets
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.count
+    return @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+  
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+  
+  def sell_pets 
+    @pets.each do |pet_type, pets|
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+      pets.clear
+    end
+  end
+  
+  def list_pets
+    # Too lazy to make this more intelligent for now. COME BACK TO THIS
+    return "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+      
 end
